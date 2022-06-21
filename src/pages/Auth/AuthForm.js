6f8339bd278e4e20046cc1a25e0bfb96c2270302
Dmitry220 <dmitry19.kravtsov123@gmail.com>
2022-06-21 15:04:04 +0300

    fix of login form

--- src/pages/Auth/AuthForm.js
+++ src/pages/Auth/AuthForm.js
@@ -1,71 +1,60 @@
-import React from 'react'
-import { Field, Form, Formik } from 'formik'
-import { useNavigate } from 'react-router-dom'
+import React, {useState} from 'react'
+import {Field, Form, Formik} from 'formik'
+import {useNavigate} from 'react-router-dom'
 import './authForm.css'
-import { Navigate } from 'react-router-dom'
-
-const validatePassword = (value) => {
-  if (value !== 'qwerty123') {
-    return 'Проверьте правильность ввода пароля'
-  }
-}
-
-const validateUsername = (value) => {
-  if (value !== 'admin') {
-    return 'Проверьте правильность ввода логина'
-  }
-}
+import {Navigate} from 'react-router-dom'
 
 export const AuthForm = () => {
-  const navigate = useNavigate()
+   const navigate = useNavigate()
+   const [errors, setErrors] = useState(null)
 
-  const submit = () => {
-    localStorage.setItem('auth', JSON.stringify(true))
-    navigate('/admin')
-  }
-  let isAuthorized = JSON.parse(localStorage.getItem('auth'))
+   let isAuthorized = JSON.parse(localStorage.getItem('auth'))
 
-  if (isAuthorized) return <Navigate to='/admin' />
+   if (isAuthorized) return <Navigate to='/admin'/>
 
-  return (
-    <div className={'auth'}>
-      <h1 className={'auth__title'}>Авторизация</h1>
-      <Formik
-        initialValues={{
-          username: '',
-          password: ''
-        }}
-        onSubmit={() => submit()}
-      >
-        {({ errors, touched, isValidating }) => (
-          <Form className={'auth__form'}>
-            <Field
-              className={'auth__field'}
-              name='username'
-              validate={validateUsername}
-              placeholder={'Логин'}
-            />
-            {errors.username && touched.username && (
-              <div className={'auth__error'}>{errors.username}</div>
-            )}
+   return (
+     <div className={'auth'}>
+        <h1 className={'auth__title'}>Авторизация</h1>
+        <Formik
+          initialValues={{
+             username: '',
+             password: ''
+          }}
+          onSubmit={async (values) => {
+             if (values.username !== 'admin' || values.password !== '1234') {
+                setErrors('Не верный логин или пароль')
+             } else {
+                localStorage.setItem('auth', JSON.stringify(true))
+                navigate('/admin')
+             }
+          }}
+        >
 
-            <Field
-              className={'auth__field'}
-              type={'password'}
-              name='password'
-              validate={validatePassword}
-              placeholder={'Пароль'}
-            />
-            {errors.password && touched.password && (
-              <div className={'auth__error'}>{errors.password}</div>
-            )}
+           <Form className={'auth__form'}>
+              <Field
+                className={'auth__field'}
+                name='username'
+                placeholder={'Логин'}
+              />
+              {/*{errors.username && (*/}
+              {/*  <div className={'auth__error'}>{errors.username}</div>*/}
+              {/*)}*/}
+              <Field
+                className={'auth__field'}
+                type={'password'}
+                name='password'
+                placeholder={'Пароль'}
+              />
+              {errors && (
+                <div className={'auth__error'}>{errors}</div>
+              )}
 
-            <button className={'auth__button'} type='submit'>
-              Войти
-            </button>
-          </Form>
-        )}
-      </Formik>
-    </div>
-  )
+              <button className={'auth__button'} type='submit'>
+                 Войти
+              </button>
+           </Form>
+
+        </Formik>
+     </div>
+   )
 }
